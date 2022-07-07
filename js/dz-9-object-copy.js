9ffe39cee7aaf704d650358b0f2334a8248072b1
Maksym.info <nagornymaxim@gmail.com>
2022-07-07 16:12:58 +0300

    added dz-9

--- js/dz-9-object-copy.js
+++ js/dz-9-object-copy.js
@@ -11,20 +11,53 @@ const object1 = {
     }
 }
 
-
 const newObj = JSON.parse(JSON.stringify(object1)); // var 1
-for (let key in object1) { // Добавил туда еще функцию от себя, для практики, но пока не смог додуматься как автоматизировать копирование функции, которая не на первом уровне вложенности объекта
+for (let key in object1) { // // Добавил туда еще функцию от себя, для практики, но пока не смог додуматься как автоматизировать копирование функции, которая не на первом уровне вложенности объекта
     if (typeof object1[key] === 'function') {
         newObj[key] = object1[key];
     }
 }
 
+console.log('============= var1 ====================')
 
-
-console.log(newObj);
-newObj.a = 10;
 newObj.b.g = 1000;
 newObj.b.t.q = 3000;
-console.log('====================');
-console.log(object1);
+console.log('copied', newObj);
 
+console.log('original', object1);
+
+
+// Вариант 2
+console.log('============= var2 ====================')
+const newObj2 = {};
+function objFind (copiedObject) {
+    for (let key in this) {
+        typeof this[key] === 'object' ? copiedObject[key] = {...this[key]} : copiedObject[key] = this[key];
+    }
+
+}
+objFind.bind(object1)(newObj2);
+objFind.bind(object1.b)(newObj2.b);
+
+console.log('copied var2', newObj2);
+newObj2.b.g = 1000;
+newObj2.b.t.q = 3000;
+console.log('original var2', object1);
+
+// function objFind (copiedObject) {
+//     for (let key in this) {
+//         // typeof this[key] === 'object' ? copiedObject[key] = {...this[key]} : copiedObject[key] = this[key];
+//         if (typeof this[key] === 'object') {
+//             copiedObject[key] =
+//             for (let innerKey in this[key]) {
+//                 if (typeof this[key][innerKey] === 'object') {
+//                     copiedObject[key][innerKey] = {...this[key][innerKey]};
+//                 }
+//                 // else {copiedObject[key][innerKey] = this[key][innerKey]}
+//
+//             }
+//         } else { copiedObject[key] = this[key] }
+//     }
+//     return copiedObject;
+// }
+// objFind.bind(object1)(newObj);
